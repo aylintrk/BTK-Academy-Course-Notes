1bf2cd024ef5b84a8ba0fdd3f1eda645339544ae
Aylin Turk <aylinn_turk@hotmail.com>
2022-08-06 15:19:40 +0300

    First commit

--- Findthenumbergame.py
+++ Findthenumbergame.py
@@ -1,19 +1,20 @@
-# 1-1000 arasında sayı var.Bulana kadar deniyelim. Bilgisayar girilen sayıyı asıl sayıdan büyük mü küçük mü diye belirlesin.
-#oyunun kaç hamlede tamamlandığını ekrana basınız.
+
+# There are numbers between 1-1000. Let's try until we find it. Let the computer determine whether the entered number is greater or less than the original number.
+#press the screen in how many moves the game is completed.
 import random
 count=0
 num= random.randint(0,10)
 while True:
     guess= int(input("Enter a number: "))
     if guess== num:
-        print("Tebrikler bildiniz!")
+        print("Congrats!You've found it!")
         count += 1
         break
     elif guess>num :
-        print("Daha küçük")
+        print("Go for smaller")
         count += 1
     else:
-        print("Daha büyük")
+        print("Go for higher")
         count += 1
-print("",count,"hamlede tahmin ettiniz.")
-print("Oyunu bitir.")
+print("You've guessed it in your","",count,". move!")
+print("You've finished the game!!.")
