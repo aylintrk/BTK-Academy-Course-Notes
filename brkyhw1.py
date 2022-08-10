d1cfc94e61f858bbe7c9b01c88bde7bc566a93a8
aylintrk <110416353+aylintrk@users.noreply.github.com>
2022-08-10 12:38:57 +0300

    Delete brkyhw1.py

--- brkyhw1.py
+++
@@ -1,6 +0,0 @@
-# En çok kullanılan harfi metin içerisinden bul sayısını bastır.
-line= input("enter the string: ")
-alphabet= [abcdefghIjklmnopqrstuvwxyz]
-for i in alphabet:
-        for i in range(len(line))
-#En az kullanılan harfle bir kelime bul.
